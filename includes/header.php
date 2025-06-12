9852c29ff478a892296ec90e03ae1bf14dffe7f6
XaviLG <91658981+XaviLG@users.noreply.github.com>
2025-06-12 21:29:49 +0200

    update header.php
    Eliminar mi cuenta, no tendra funcionalidad

--- includes/header.php
+++ includes/header.php
@@ -34,7 +34,6 @@
     <div class="header-right">
         <a href="/portaFilm/pages/lista.php">Lista</a>
         <?php if (isset($_SESSION['usuario_id'])): ?>
-    <a href="/portaFilm/pages/home.php">Mi cuenta</a>
     <a href="/portaFilm/controllers/logout.php">Cerrar sesión</a>
 
     <?php if ($_SESSION['usuario_rol'] === 'admin'): ?>
